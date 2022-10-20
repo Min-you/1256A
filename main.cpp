72631639c88c426a5079f8697cb960bb25b88e54
Min-you <101444459+Min-you@users.noreply.github.com>
2022-10-20 19:47:33 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
 	while (q--){
 		int a, b, n, s;
 		cin >> a >> b >> n >> s;
-		int sum = s%n;
+		int sum = s%n; //the question wants both coins a and b to be included
 		if (sum <= b && 1ll * a * n + b >= s){
 			printf("YES\n");
 		}
